cd2016fe4427235b7eb85112dc2fd3da6dbb3c44
argezdev <mateo@spatio.app>
2022-05-09 20:03:56 -0400

    Static Files added

--- README.md
+++
@@ -1,2 +0,0 @@
-# ParallaxFadeIn
-React - Three.js parallax scroll effect while elements fade in smoothly.
---
+++ precache-manifest.f1baeea2c153bc61b6b24d905bf5b242.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170dc18be70f2f8960c33b077ef71628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43845d07ffafdf96b80",
+    "url": "/static/css/main.db03f86e.chunk.css"
+  },
+  {
+    "revision": "bd4486f438ec21f1917e",
+    "url": "/static/js/2.17c111ed.chunk.js"
+  },
+  {
+    "revision": "d43845d07ffafdf96b80",
+    "url": "/static/js/main.0e042586.chunk.js"
+  },
+  {
+    "revision": "80b789232f75104194ee",
+    "url": "/static/js/runtime-main.e2fb13b6.js"
+  }
+]);
