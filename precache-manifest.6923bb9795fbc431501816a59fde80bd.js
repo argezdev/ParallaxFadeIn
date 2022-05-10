32a5528d1ea404d714573a907e887d1c81f5d9fe
argezdev <mateo@spatio.app>
2022-05-09 21:17:20 -0400

    Updates

---
+++ precache-manifest.6923bb9795fbc431501816a59fde80bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cec0af7228fab9a018e1252d243ca6c",
+    "url": "/ParallaxFadeIn/index.html"
+  },
+  {
+    "revision": "f91e81e51a638603b535",
+    "url": "/ParallaxFadeIn/static/css/main.db03f86e.chunk.css"
+  },
+  {
+    "revision": "82c57ed088d9498540a7",
+    "url": "/ParallaxFadeIn/static/js/2.768c52eb.chunk.js"
+  },
+  {
+    "revision": "f91e81e51a638603b535",
+    "url": "/ParallaxFadeIn/static/js/main.e2f19c3a.chunk.js"
+  },
+  {
+    "revision": "05624786ae36ef11b483",
+    "url": "/ParallaxFadeIn/static/js/runtime-main.6a8ae6aa.js"
+  }
+]);
