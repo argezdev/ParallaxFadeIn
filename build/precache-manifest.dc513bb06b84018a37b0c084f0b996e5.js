56b33ef99ff5fed076d6eb247575473ab5a2b974
argezdev <mateo@spatio.app>
2022-05-09 22:23:01 -0400

    Fixed Dependencies and Page

---
+++ build/precache-manifest.dc513bb06b84018a37b0c084f0b996e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705729585ad988e887f6018d113130c0",
+    "url": "/ParallaxFadeIn/index.html"
+  },
+  {
+    "revision": "830d8b6a9056cc4d8077",
+    "url": "/ParallaxFadeIn/static/css/main.db03f86e.chunk.css"
+  },
+  {
+    "revision": "51a7b9c62b8e3247e9bf",
+    "url": "/ParallaxFadeIn/static/js/2.1a4e2c0d.chunk.js"
+  },
+  {
+    "revision": "830d8b6a9056cc4d8077",
+    "url": "/ParallaxFadeIn/static/js/main.9536eb35.chunk.js"
+  },
+  {
+    "revision": "05624786ae36ef11b483",
+    "url": "/ParallaxFadeIn/static/js/runtime-main.6a8ae6aa.js"
+  }
+]);
